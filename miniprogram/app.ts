97c102a7435c42aa69cc9ef70d6cec2af747e3cc
南秋SouthAki <xieleihan@gmail.com>
2024-11-01 22:09:37 +0800

    feat:购物车页大抵完成

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,6 +1,27 @@
 // app.ts
-App<IAppOption>({
-  globalData: {},
+App({
+  globalData: {
+    shopcarList:[
+      {
+        "pic":"https://picsum.photos/300/300?1",
+        "title":"这是标题1",
+        "picel":"259.99",
+        "num":"1"
+      },
+      {
+        "pic":"https://picsum.photos/300/300?2",
+        "title":"这是标题2",
+        "picel":"256.99",
+        "num":"1"
+      },
+      {
+        "pic":"https://picsum.photos/300/300?3",
+        "title":"这是标题3",
+        "picel":"239.33",
+        "num":"1"
+      }
+    ]
+  },
   onLaunch() {
     
   },
--- miniprogram/pages/shopcar/shopcar.ts
+++ miniprogram/pages/shopcar/shopcar.ts
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    shopcarList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    const app = getApp();
+    this.setData({
+      shopcarList : app.globalData.shopcarList
+    })
   },
 
   /**
@@ -40,7 +43,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    
   },
 
   /**
@@ -62,5 +65,7 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+
+
 })
