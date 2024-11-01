9821751ed9d62fe2ef34727a318ed4f0acf2958e
南秋SouthAki <xieleihan@gmail.com>
2024-11-01 17:24:12 +0800

    feat:分类页大抵完成

--- miniprogram/pages/category/category.ts
+++ miniprogram/pages/category/category.ts
@@ -1,18 +1,76 @@
 // pages/category/category.ts
+import {url} from "../../api/index"
+import {get} from "../../api/request"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    provinces:[],
+    selectIndex: 0,
+    selectCity:"北京市",
+    cittyList:[]
+  },
 
+  onItemTap: function(event: { currentTarget: { dataset: { index: any; }; }; }) {
+    const index = event.currentTarget.dataset.index;
+    this.setData({
+      selectedIndex: index
+    });
+    console.log(this.data.provinces[index])
+    this.setData({
+      selectCity:this.data.provinces[index]
+    })
+
+    const urlcity = url +"/public/cities"
+    const data ={"province":this.data.selectCity}
+    this.cityData(urlcity,data)
+  },
+
+  async fetchData(urlStr:string){
+    try{
+      const url = urlStr;
+      console.log(url)
+      const response = await get(url);
+      console.log(response.provinces)
+      this.setData({
+        provinces: response.provinces
+      })
+      
+    }catch{
+      console.log("error")
+    }
+  },
+
+  async cityData(urlStr:string,data:object){
+    try{
+      const url = urlStr;
+      console.log(url,data)
+      const response = await get(url,data);
+      console.log(response)
+      this.setData({
+        cittyList: response.cities
+      })
+    }catch{
+      console.log("error")
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
+    const urlStr = url +"/public/provinces"
+    this.fetchData(urlStr)
+    this.setData({
+      selectedIndex: 0
+    });
 
+    const urlcity = url +"/public/cities"
+    const data ={"province":this.data.selectCity}
+    this.cityData(urlcity,data)
   },
 
   /**
