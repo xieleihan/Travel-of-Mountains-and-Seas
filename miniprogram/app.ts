ff4a0cce00ab60de9b651bda19a77e0ce01ff1c0
xieleihan <xieleihan@gmail.com>
2024-11-02 11:41:35 +0800

    feat:购物车功能基本实现

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -6,21 +6,31 @@ App({
         "pic":"https://picsum.photos/300/300?1",
         "title":"这是标题1",
         "picel":"259.99",
-        "num":"1"
+        "num":"1",
+        "totalPrice":"259.99",
+        "isSelected":false
       },
       {
         "pic":"https://picsum.photos/300/300?2",
         "title":"这是标题2",
         "picel":"256.99",
-        "num":"1"
+        "num":"1",
+        "totalPrice":"256.99",
+        "isSelected":false
       },
       {
         "pic":"https://picsum.photos/300/300?3",
         "title":"这是标题3",
         "picel":"239.33",
-        "num":"1"
+        "num":"1",
+        "totalPrice":"239.33",
+        "isSelected":false
       }
-    ]
+    ],
+    count:{
+      sumnum:0,
+      sumtotel:0
+    }
   },
   onLaunch() {
     
--- miniprogram/pages/shopcar/shopcar.ts
+++ miniprogram/pages/shopcar/shopcar.ts
@@ -1,11 +1,96 @@
 // pages/shopcar/shopcar.ts
+interface ShopcarItem {
+  picel: number | string;
+  num: number | string;
+  totalPrice?: number | string;
+  isSelected:boolean
+}
+
+interface countItem{
+  sumnum: number;
+  sumtotel: number | string;
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    shopcarList: []
+    shopcarList: [] as ShopcarItem[],
+    isAllopen:false, // 全选开关
+    isSumnum:0, // list总数
+    all:false, // 全选复选框的开关
+    count:{} as countItem
+  },
+
+  // 单选判断是否开启全选
+  isOpenAll(event: WechatMiniprogram.TouchEvent){
+    const index = event.currentTarget.dataset.index;
+    // console.log(index,event)
+    const shopcarList = this.data.shopcarList;
+
+    // 切换选中状态
+    shopcarList[index].isSelected = !shopcarList[index].isSelected;
+
+    // 更新数量和总价
+    let totalNum = 0;
+    let totalPrice = 0;
+
+    shopcarList.forEach(item => {
+      if (item.isSelected) {
+        totalNum += Number(item.num); // 确保将字符串转换为数字
+        totalPrice += Number(item.totalPrice);
+      }
+    });
+
+    // 更新全选的总数
+    const selectedCount = shopcarList.filter(item => item.isSelected).length;
+    this.setData({
+        shopcarList: shopcarList,
+        isSumnum: selectedCount,
+        count: {
+          sumnum: totalNum,
+          sumtotel: totalPrice.toFixed(2) // 更新总价格
+        },
+        all: selectedCount === shopcarList.length // 判断是否全选
+    });
+
+    // 更新全选状态
+    this.setData({
+        isAllopen: this.data.all
+    });
+  },
+
+  // 全选的开关
+  toggleAllopen(){
+    const newIsAllOpen = !this.data.isAllopen;
+    const shopcarList = this.data.shopcarList.map(item => ({
+        ...item,
+        isSelected: newIsAllOpen // 全选时设置所有项为选中或未选中
+    }));
+
+    // 计算全选后的总数和总价
+    let totalNum = 0;
+    let totalPrice = 0;
+
+    if (newIsAllOpen) {
+      shopcarList.forEach(item => {
+        totalNum += Number(item.num);
+        totalPrice += Number(item.totalPrice);
+      });
+    }
+
+    this.setData({
+        shopcarList: shopcarList,
+        isAllopen: newIsAllOpen,
+        all: newIsAllOpen,
+        count: {
+          sumnum: totalNum,
+          sumtotel: totalPrice.toFixed(2) // 更新总价格
+        },
+        isSumnum: newIsAllOpen ? shopcarList.length : 0 // 更新总数
+    });
   },
 
   /**
@@ -13,9 +98,22 @@ Page({
    */
   onLoad() {
     const app = getApp();
+    const count = app.globalData.count
+    // console.log(count)
+    const shopcarList = app.globalData.shopcarList.map((item:any) => {
+      return {
+        ...item,
+        totalPrice: (Number(item.picel) * Number(item.num)).toFixed(2)
+      };
+    });
+    // console.log(shopcarList)
     this.setData({
-      shopcarList : app.globalData.shopcarList
+      shopcarList : shopcarList,
+      isSumnum: shopcarList.length,
+      count: count
     })
+    console.log(this.data.count)
+    // console.log(this.data.isSumnum)
   },
 
   /**
@@ -67,5 +165,48 @@ Page({
 
   },
 
+  sub(event: WechatMiniprogram.TouchEvent) {
+    const index = event.currentTarget.dataset.index;
+    console.log(index)
+    const shopcarList = this.data.shopcarList;
+    if (shopcarList[index].num > 1) {
+      shopcarList[index].num = Number(shopcarList[index].num) - 1; // 转换为数字
+      shopcarList[index].totalPrice = (Number(shopcarList[index].picel) * Number(shopcarList[index].num)).toFixed(2);
+      this.setData({
+        shopcarList: shopcarList
+      });
+    }
+    this.updateCount();
+  },
+  
+  plus(event: WechatMiniprogram.TouchEvent) {
+    const index = event.currentTarget.dataset.index;
+    const shopcarList = this.data.shopcarList;
+    shopcarList[index].num = Number(shopcarList[index].num) + 1; // 转换为数字
+    shopcarList[index].totalPrice = (Number(shopcarList[index].picel) * Number(shopcarList[index].num)).toFixed(2);
+    this.setData({
+      shopcarList: shopcarList
+    });
+    this.updateCount();
+  },
 
+  updateCount() {
+    const shopcarList = this.data.shopcarList;
+    let totalNum = 0;
+    let totalPrice = 0;
+
+    shopcarList.forEach(item => {
+      if (item.isSelected) {
+        totalNum += Number(item.num);
+        totalPrice += Number(item.totalPrice);
+      }
+    });
+
+    this.setData({
+      count: {
+        sumnum: totalNum,
+        sumtotel: totalPrice.toFixed(2) // 确保总价格保留两位小数
+      }
+    });
+  }
 })
